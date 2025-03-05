f7cb82af44a5c8d344cda59541278fe2e0762d40
AdamFakee <buidinhtuan04@gmail.com>
2025-03-05 21:18:17 +0700

    refactor: thêm 1 số trường trả về từ backEnd

--- src/controllers/client/enrollment.controller.js
+++ src/controllers/client/enrollment.controller.js
@@ -3,15 +3,20 @@
 const { BadRequestError } = require("../../core/error.response");
 const { OK } = require("../../core/success.response");
 const { getOneCourseByCourseKey } = require("../../services/course.service");
-const { createEnrollment } = require("../../services/enrollment.service");
+const { createEnrollment, getEnrollmentByUserEmail } = require("../../services/enrollment.service");
 
 const _createEnrollment = async ( req, res, next ) => {
     const { key } = req.body;
+    console.log(key)
     if(!key) {
         throw new BadRequestError('Error::: key is required')
     }
 
     const courseWithKey = await getOneCourseByCourseKey(key);
+    if(!courseWithKey.length) {
+        throw new BadRequestError('Error::: course key is not valid')
+    }
+
     const { courseId, keyCode } = courseWithKey[0];
     if(!courseId ||!keyCode) {
         throw new BadRequestError('Error::: invalid key')
@@ -22,6 +27,9 @@ const _createEnrollment = async ( req, res, next ) => {
         throw new BadRequestError('Error::: email is required')
     }
 
+    const existRollWithEmail = await getEnrollmentByUserEmail(email, courseId);
+    if(existRollWithEmail) throw new BadRequestError('Error::: you have already enrolled this course')
+
     const data = {
         course_id: courseId,
         user_email: email,
@@ -29,7 +37,10 @@ const _createEnrollment = async ( req, res, next ) => {
     }
     await createEnrollment(data);
 
-    return new OK({}).send(res)
+    const metadata = { courseId };
+    return new OK({
+        metadata
+    }).send(res)
 
 }
 
